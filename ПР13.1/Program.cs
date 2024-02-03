47be5c1d9d3a022f6b5b5b811b87cb71c355a071
vafina.ar2094 <vafina.ar2094@COM403-L3>
2024-02-03 10:00:03 +0500

    Добавьте файлы проекта.

---
+++ ПР13.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР13._1
+{
+    class Animals
+    {
+        public string Name { get; set; }
+        public void Eat()
+        {
+            Console.WriteLine("Кушает");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Спит");
+        }
+    }
+    class Dog : Animals
+    {
+        public void Play()
+        {
+            Console.WriteLine("Играет");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Name = Console.ReadLine();
+            dog.Play();
+            dog.Eat();
+            dog.Sleep();
+        }
+    }
+}
